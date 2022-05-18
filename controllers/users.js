a87ebe1690d3f34d3e020e9aaeb8f1e9fedeb2ed
Elwoode <elwoode@yandex.ru>
2022-05-18 17:16:02 +0300

    спринт 13

--- controllers/users.js
+++ controllers/users.js
@@ -29,15 +29,13 @@ const getUser = (req, res) => {
 const createUser = (req, res) => {
   const { name, about, avatar } = req.body;
   return User.create({ name, about, avatar })
-    .then((user) => {
-      res.status(201).send(user)
-        .catch((err) => {
-          if (err.name === 'ValidationError' || err.name === 'CastError') {
-            res.status(400).send({ message: 'Переданы некорректные данные при создании пользователя' });
-          } else {
-            res.status(500).send({ message: 'На сервере произошла ошибка' });
-          }
-        });
+    .then((user) => res.status(200).send(user))
+    .catch((err) => {
+      if (err.name === 'ValidationError' || err.name === 'CastError') {
+        res.status(400).send({ message: 'Переданы некорректные данные при создании пользователя' });
+      } else {
+        res.status(500).send({ message: 'На сервере произошла ошибка' });
+      }
     });
 };
 
