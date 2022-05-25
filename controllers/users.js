2437ee41adcde153a581f0752eb761c14d422322
Elwoode <elwoode@yandex.ru>
2022-05-25 19:13:37 +0300

    спринт 14

--- controllers/users.js
+++ controllers/users.js
@@ -34,7 +34,11 @@ const getUser = (req, res, next) => {
 const createUser = (req, res, next) => {
   bcrypt.hash(req.body.password, 10)
     .then((hash) => User.create({
-      name: req.body.name, about: req.body.about, avatar: req.body.avatar, email: req.body.email, password: hash,
+      name: req.body.name,
+      about: req.body.about,
+      avatar: req.body.avatar,
+      email: req.body.email,
+      password: hash,
     }))
     .then((user) => {
       res.status(201).send({
